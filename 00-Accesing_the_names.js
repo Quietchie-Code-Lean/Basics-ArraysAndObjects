814ad303e3846fb88e4ef9468195f2e28b043f5b
Leandro Funes <leofunesar@gmail.com>
2025-12-23 11:36:48 +0000

    adding Excersices Unit arrays and objects, first approaching self-learning guide

---
+++ 00-Accesing_the_names.js
@@ -0,0 +1,42 @@
+/* Array Persian Names */
+let persianNames = [
+    "Darius",
+    "Cyrus",
+    "Arman",
+  "Ramin",
+  "Kian",
+  "Farid",
+  "Nima",
+  "Soroush",
+  "Navid",
+  "Arya"
+];
+
+/* Array Latin Names */
+
+let latinNames = [
+    "Marcus",
+    "Julius",
+    "Lucius",
+    "Aurelius",
+    "Octavius",
+    "Flavius",
+    "Maximus",
+    "Tiberius",
+    "Valerius",
+    "Cassius"
+];
+
+console.log("Display 3 persian names in the console using indexes 0, 3, and 5.")
+
+console.log(persianNames[0]);
+console.log(persianNames[3]);
+console.log(persianNames[5]);
+
+console.log(`Display the name ${latinNames[3]} in the console using its corresponding index`)
+
+console.log(latinNames[3]);
+
+console.log("Display the last element using its corresponding index")
+
+console.log(latinNames[latinNames.length-1])
---
+++ 01-loop_for_of.js
@@ -0,0 +1,5 @@
+const seasons = ["Summer", "Autumn", "Winter", "Spring"];
+
+for(season of seasons){
+console.log(season);
+};

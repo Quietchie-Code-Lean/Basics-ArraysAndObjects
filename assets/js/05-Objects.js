fa06cf35da99deac8de2945d7f387cf7eb636fde
Leandro Funes <leofunesar@gmail.com>
2025-12-26 18:19:44 +0000

    modifying objects within project

---
+++ assets/js/05-Objects.js
@@ -0,0 +1,48 @@
+/* Create Objects: */
+
+/* Array de objetos: */
+
+let guitars = [
+  {
+    id: "p-1",
+    brand: "Fender",
+    model: "Stratocaster",
+    year: 1965,
+    color: "Sunburst"
+  },
+  {
+    id: "p-2",
+    brand: "Gibson",
+    model: "Les Paul Standard",
+    year: 1959,
+    color: "Cherry Sunburst"
+  },
+  {
+    id: "p-3",
+    brand: "Ibanez",
+    model: "RG550",
+    year: 1987,
+    color: "Road Flare Red"
+  },
+  {
+    id: "p-4",
+    brand: "PRS",
+    model: "Custom 24",
+    year: 1985,
+    color: "Emerald Green"
+  },
+  {
+    id: "p-5",
+    brand: "Yamaha",
+    model: "Pacifica 112V",
+    year: 2018,
+    color: "Sonic Blue"
+  },
+  {
+    id: "p-6",   
+    brand: "ESP",
+    model: "Eclipse",
+    year: 2005,
+    color: "Vintage Black"
+  }
+];
